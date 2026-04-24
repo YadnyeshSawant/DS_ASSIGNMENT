40aa8e25f10963757d1b037d457ccb7e6e1cd781
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2026-04-24 11:06:47 +0530

    Add screenshots and update title formatting in app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ data = pickle.load(open('ames_model.pkl', 'rb'))
 model = data['model']
 features = data['features']
 
-st.title("House Price Prediction (Ames Dataset)")
+st.title("House Price Prediction")
+st.title("(Ames Dataset)")
 
 st.write("Enter basic house details:")
 
